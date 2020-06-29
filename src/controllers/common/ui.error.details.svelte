6832b2b6780cf704d9ea6656466cea56f287c2e8
Aleksander Repin <alex.repin@mail.ru>
2020-06-29 18:00:39 +0300

    fix for details

--- src/controllers/common/ui.error.details.svelte
+++ src/controllers/common/ui.error.details.svelte
@@ -5,27 +5,27 @@
 <h2 class="h2">Ошибка {item.errorID}</h2>
 <div class="container">
   <div class="columns">
-    <div class="column">Название</div>
+    <div class="column is-one-fifth">Название</div>
     <div class="column">{item.details.name}</div>
   </div>
   <div class="columns">
-    <div class="column">Сообщение</div>
+    <div class="column is-one-fifth">Сообщение</div>
     <div class="column">{item.details.message}</div>
   </div>
   <div class="columns">
-    <div class="column">Стэк вызовов</div>
-    <div class="column">{item.details.stack}</div>
+    <div class="column is-one-fifth">Стэк вызовов</div>
+    <div class="column"><pre>{@html item.details.stack}</pre></div>
   </div>
   <div class="columns">
-    <div class="column">Дополнительная информация</div>
-    <div class="column">{item.options}</div>
+    <div class="column is-one-fifth">Дополнительная информация</div>
+    <div class="column"><pre>{@html item.options}</pre></div>
   </div>
   <div class="columns">
-    <div class="column">Окружение</div>
-    <div class="column">{item.env}</div>
+    <div class="column is-one-fifth">Окружение</div>
+    <div class="column"><pre>{@html item.env}</pre></div>
   </div>
   <div class="columns">
-    <div class="column">Дата создания</div>
+    <div class="column is-one-fifth">Дата создания</div>
     <div class="column">{item.createdAt}</div>
   </div>
 
